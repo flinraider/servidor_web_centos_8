a8fc61591f17fd6d38ac5ef48c6d271597c12a68
Leonardo Teixeira <69474499+flinraider@users.noreply.github.com>
2022-05-24 10:42:32 -0300

    Create install_lamp.sh

---
+++ install_lamp.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+
+# install apache-2 mysql-server-8.0.26 and php-7.4 
+
+sudo yum update -y
+
+sudo yum install httpd mysql-server -y
+
+sudo yum module list php
+
+sudo module enable php:7.4 -y
+
+sudo yum install -y php php-pdo php-pecl-zip php-json php-common php-fpm php-mbstring php-cli php-mysqlnd php-gd  php-xml php-intl unzip  
+
+sudo systemctl enable httpd
+sudo systemctl enable mysqld
+sudo systemctl start httpd
+sudo systemctl start mysqld
+
+sudo mysql_secure_installation
+
+
+
